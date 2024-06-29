fe400fa3ae82fefcbdc2e15cebf5277461bb49e6
Mr.UNIX <mrunix00@protonmail.com>
2024-06-29 19:39:53 +0100

    Add support for configuring Redis cache

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ import (
 type Configuration struct {
 	listenHostname string
 	listenPort     string
+	redisHostname  string
+	redisPort      string
+	redisUsername  string
+	redisPassword  string
+	redisDB        int
 }
 
 type SPError struct {
@@ -28,12 +33,36 @@ func getConfiguration() (*Configuration, error) {
 	var config Configuration
 	config.listenHostname = os.Getenv("SP_HOSTNAME")
 	config.listenPort = os.Getenv("SP_PORT")
+	config.redisHostname = os.Getenv("SP_REDIS_HOSTNAME")
+	config.redisPort = os.Getenv("SP_REDIS_PORT")
+	config.redisUsername = os.Getenv("SP_REDIS_USERNAME")
+	config.redisPassword = os.Getenv("SP_REDIS_PASSWORD")
 
 	if len(config.listenPort) == 0 {
 		return nil, SPError{"SP_PORT environment variable is not set!"}
 	}
 	if portInt, err := strconv.Atoi(config.listenPort); err != nil || portInt > 65535 {
 		return nil, SPError{"SP_PORT is not a valid port number!"}
+	}
+
+	if len(config.redisHostname) == 0 {
+		log.Println("[-] SP_REDIS_HOSTNAME is not set, defaulting to localhost...")
+		config.listenHostname = "localhost"
+	}
+	if len(config.redisPort) == 0 {
+		log.Println("[-] SP_REDIS_PORT is not set, defaulting to :6379...")
+		config.redisPort = "6379"
+	}
+	if portInt, err := strconv.Atoi(config.redisPort); err != nil || portInt > 65535 {
+		return nil, SPError{"SP_REDIS_PORT is not a valid port number!"}
+	}
+
+	if len(os.Getenv("SP_REDIS_DB")) == 0 {
+		config.redisDB = 0
+	} else if db, err := strconv.Atoi(os.Getenv("SP_REDIS_DB")); err == nil {
+		config.redisDB = db
+	} else {
+		return nil, SPError{"SP_REDIS_DB is not a valid integer!"}
 	}
 
 	return &config, nil
@@ -127,9 +156,10 @@ func copyHeader(dst, src http.Header) {
 func startProxy(config Configuration) {
 	hostAddr := net.JoinHostPort(config.listenHostname, config.listenPort)
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
+		Addr:     net.JoinHostPort(config.redisHostname, config.redisPort),
+		Username: config.redisUsername,
+		Password: config.redisPassword,
+		DB:       config.redisDB,
 	})
 	ctx := context.Background()
 	server := http.Server{
